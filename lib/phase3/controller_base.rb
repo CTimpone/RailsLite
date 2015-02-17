a2ba8e2380c7802238da5f078832b4714521fe4e
Chris Timpone <christimpone@gmail.com>
2015-02-17 12:48:50 -0500

    Add template evaluation (Phase3) complete with spec passing.

--- lib/phase3/controller_base.rb
+++ lib/phase3/controller_base.rb
@@ -1,5 +1,6 @@
 require_relative '../phase2/controller_base'
 require 'active_support'
+require 'active_support/inflector'
 require 'active_support/core_ext'
 require 'erb'
 
@@ -8,6 +9,12 @@ module Phase3
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
     def render(template_name)
+      snake_class = self.class.to_s.underscore
+      path = "views/#{snake_class}/#{template_name.to_s}.html.erb"
+      read_file = File.read(path, :encoding => 'iso-8859-1')
+      content = ERB.new(read_file).result(binding)
+      content_type = "text/html"
+      render_content(content, content_type)
     end
   end
 end
