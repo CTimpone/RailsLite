ec7aa89aa7aea6960083c1578ad3c34d87544712
Chris Timpone <christimpone@gmail.com>
2015-02-17 15:44:06 -0500

    Build base router, no param considerations.

--- lib/phase6/controller_base.rb
+++ lib/phase6/controller_base.rb
@@ -4,6 +4,15 @@ module Phase6
   class ControllerBase < Phase5::ControllerBase
     # use this with the router to call action_name (:index, :show, :create...)
     def invoke_action(name)
+      send(name)
+
+      unless already_built_response?
+        render(name)
+      else
+        raise "Already built response."
+      end
+
+      @already_built_response = true
     end
   end
 end
