5f5a2ed1c4980e1f548102deae3ea74793a4144e
Chris Timpone <christimpone@gmail.com>
2015-02-17 12:18:40 -0500

    Construct simple ControllerBase that passes phase 2 specs.

--- lib/phase2/controller_base.rb
+++ lib/phase2/controller_base.rb
@@ -4,20 +4,39 @@ module Phase2
 
     # Setup the controller
     def initialize(req, res)
+      @req = req
+      @res = res
     end
 
     # Helper method to alias @already_built_response
     def already_built_response?
+      @already_built_response ||= false
     end
 
     # Set the response status code and header
     def redirect_to(url)
+      unless already_built_response?
+        @res.status = 302
+        @res.header["location"] = url
+      else
+        raise "Already redirected."
+      end
+
+      @already_built_response = true
     end
 
     # Populate the response with content.
     # Set the response's content type to the given type.
     # Raise an error if the developer tries to double render.
     def render_content(content, content_type)
+      unless already_built_response?
+        @res.content_type = content_type
+        @res.body = content
+      else
+        raise "Already rendered content."
+      end
+
+      @already_built_response = true
     end
   end
 end
